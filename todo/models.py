29924c3f392024153dd4206eeb6768bf73558963
Denys <deibukdenyscloud@gmail.com>
2023-09-29 19:29:22 +0300

    Create todo application and populate models.py

---
+++ todo/models.py
@@ -0,0 +1,43 @@
+from django.utils import timezone
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+from taggit.managers import TaggableManager
+
+
+class User(AbstractUser):
+    pass
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ("name",)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
+    due_date = models.DateField(default=timezone.now)
+    project = models.ForeignKey(
+        Project,
+        related_name="tasks",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+    )
+    user = models.ForeignKey(
+        User, related_name="tasks", on_delete=models.CASCADE
+    )
+    tags = TaggableManager()
+
+    class Meta:
+        ordering = ("due_date",)
+
+    def __str__(self) -> str:
+        return self.title
--- todo_app/settings.py
+++ todo_app/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-q&a&&zr5oxh*0psbf%0u40tm==ii)&!^bqtr!6lz%0+ynktst_"
+SECRET_KEY = (
+    "django-insecure-q&a&&zr5oxh*0psbf%0u40tm==ii)&!^bqtr!6lz%0+ynktst_"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "todo",
+    "taggit",
 ]
 
 MIDDLEWARE = [
@@ -86,16 +90,20 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+        "NAME": "django.contrib.auth.password_validation."
+        "UserAttributeSimilarityValidator",
     },
     {
-        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+        "NAME": "django.contrib.auth.password_validation."
+        "MinimumLengthValidator",
     },
     {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+        "NAME": "django.contrib.auth.password_validation."
+        "CommonPasswordValidator",
     },
     {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+        "NAME": "django.contrib.auth.password_validation."
+        "NumericPasswordValidator",
     },
 ]
 
@@ -121,3 +129,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "todo.User"
